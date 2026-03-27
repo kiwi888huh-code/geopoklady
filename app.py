e6473dd66b5f7f884ca5a6efec3dc4b1f0b200ac
kiwi888huh-code <kiwi888huh@gmail.com>
2026-03-27 21:14:30 +0100

    Update app.py
    added 24/7

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Geocaching – výběr pokladů")
 
 CACHE_TYPES = ["Traditional","Multi","Mystery","Virtual","Earthcache","Letterbox","Event","CITO","Wherigo"]
 SIZES = ["micro","small","regular","large","other"]
-ATTRIBUTES = ["děti","psi","speciální nástroj","drive-in","vyhlídka"]
+ATTRIBUTES = ["děti","psi","speciální nástroj","drive-in","vyhlídka","24/7"]
 
 # ====== LOAD ======
 if "treasures" not in st.session_state:
